cd83ac824bc713c6e2dc9ba53579f0a6a0a4f1f7
harrdesai <harrdesai@gmail.com>
2025-05-11 14:49:55 +0530

    feat: implement getAllSubmissions to fetch submissions by authenticated user
    - Queries all submissions from the database where userId matches the logged-in user
    - Returns structured ApiResponse with fetched data
    - Includes error handling for robust API behavior

--- backend/src/controllers/submission.controllers.js
+++ backend/src/controllers/submission.controllers.js
@@ -1,5 +1,35 @@
 // src/controllers/submission.controllers.js
-const getAllSubmissions = async (request, response) => {}
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+const getAllSubmissions = async (request, response) => {
+  try {
+
+    const userId = request.user.id;
+
+    const submissions = await prisma.submission.findMany({
+      where: {
+        userId
+      }
+    })
+
+    response.status(200).json(
+      new ApiResponse(200, submissions, "Submissions fetched successfully")
+    )
+    
+  } catch (error) {
+    
+    response.status(error.statusCode || 500).json(
+      new ApiError(error.statusCode || 500, "Error While fetching submissions", {
+        error: error.message
+      })
+    )
+
+  }
+}
 
 const getSubmissionsForProblem = async (request, response) => {}
 
