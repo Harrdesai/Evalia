4c40a4fa8cbf96f6c67e9298b43d8c3a730d53e8
harrdesai <harrdesai@gmail.com>
2025-05-29 16:48:17 +0530

    feat: Enhance authentication flow and add route protection
    - Integrated `useAuthStore` for managing authentication state and user role.
    - Added `checkAuth` method in `useAuthStore` to check user authentication status on app load.
    - Protected the `/add-problem` route with `AdminRoute` for admin access control.
    - Showed loading spinner (`lucide-react` loader) during authentication check.
    - Updated `App.tsx` to handle user login state and routing based on authentication status.
    
    # Changes include:
    - `src/App.tsx`:
      - Integrated authentication flow with `useAuthStore`.
      - Added route protection using `AdminRoute` for specific routes.
      - Displayed loading spinner while checking authentication status.
    - `src/store/useAuthStore.js`: Updated Zustand store for handling user authentication.
    - `src/components/AdminRoute.jsx`: Added logic for restricting routes to admins based on user role.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,26 @@ import { Loader } from "lucide-react";
 import HomePage from "./page/HomePage";
 import RegisterPage from "./page/register";
 import LoginPage from "./page/login";
+import { useAuthStore } from "./store/useAuthStore";
 import Layout from "./layout/layout";
+import AdminRoute from "./components/AdminRoute";
 import ProblemPage from "./page/ProblemPage";
 import AddProblem from "./page/AddProblem";
 
 function App() {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
-  let authUser = null
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10animate-spin" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-start">
@@ -39,7 +52,7 @@ function App() {
           element={authUser ? <ProblemPage /> : <Navigate to={"/login"} />}
         />
 
-        <Route >
+        <Route element={<AdminRoute />}>
           <Route
             path="/add-problem"
             element={authUser ? <AddProblem /> : <Navigate to="/" />}
