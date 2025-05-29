b1d55d8ff1e13768d5ac8a3b076d22dab646fe65
harrdesai <harrdesai@gmail.com>
2025-05-29 16:30:54 +0530

    feat(frontend): set up pages and route configuration with Vite React
    - Initialized frontend project using Vite with React and TypeScript
    - Configured development, build, lint, and preview scripts for smooth development workflow
    - Installed and set up TailwindCSS for styling, along with DaisyUI for component library
    - Integrated Monaco Editor, React Router, and Zustand for state management
    - Added React Hook Form and Zod for form handling and validation
    - Configured ESLint with TypeScript and React rules for code quality
    - Installed necessary development dependencies including TypeScript, Vite plugins, and ESLint plugins
    - Created core pages:
      - `AddProblem.jsx`
      - `HomePage.jsx`
      - `Login.jsx`
      - `ProblemPage.jsx`
      - `Register.jsx`
    - Configured routing for the pages using React Router:
      - `/` for `HomePage`
      - `/login` for `Login`
      - `/register` for `Register`
      - `/problem/:id` for `ProblemPage`
      - `/add-problem` for `AddProblem`
    
    ✅ Successfully set up React environment with routing for core pages and configured basic frontend structure for development.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,53 @@
+// src/App.tsx
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
+
+import HomePage from "./page/HomePage";
+import RegisterPage from "./page/register";
+import LoginPage from "./page/login";
+import Layout from "./layout/layout";
+import ProblemPage from "./page/ProblemPage";
+import AddProblem from "./page/AddProblem";
+
+function App() {
+
+  let authUser = null
+
+  return (
+    <div className="flex flex-col items-center justify-start">
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+        </Route>
+        <Route
+          path="/register"
+          element={!authUser ? <RegisterPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/problem/:id"
+          element={authUser ? <ProblemPage /> : <Navigate to={"/login"} />}
+        />
+
+        <Route >
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />}
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
